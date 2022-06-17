15d61f66f6b8402a59aecd37aedb9e385650122b
James_Ying <yab.james@outlook.com>
2022-06-16 22:23:32 -0400

    Add confirmation process for correct video
    Ensures that the user has inserted the correct video

--- yt-convert.py
+++ yt-convert.py
@@ -3,4 +3,21 @@ from pytube import YouTube
 youtube_link = input("Input the youtube url of the video that you \
 would to convert to audio: ")
 
+# confirmation 
+print(f'Is this the correct title of the youtube video? \n "{yt.title}" \n')
+confirm = input("Type Y/y for yes or N/n for no: ")
+proceed = False
+
+# loop through video confirmation process until user agrees
+while (proceed == False):
+
+    if (confirm.lower() == 'y'):
+        proceed = True
+
+    elif (confirm.lower() == 'n'):
+        youtube_link = input("Retype the youtube url of the video that you \
+would to convert to audio: ")
+        print(f'Is this the correct title of the youtube video? \n "{yt.title}" \n')
+        confirm = input("Type Y/y for yes or N/n for no: ")
+
 yt = YouTube(youtube_link)
