1b868442fd6bd1acd30269af3d6bc2ac0ceaf8cc
James_Ying <yab.james@outlook.com>
2022-06-16 22:05:38 -0400

    Fix youtube_link input prompt
    Prompt was previously indented

--- yt-convert.py
+++ yt-convert.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 
 youtube_link = input("Input the youtube url of the video that you \
-    would to convert to audio: ")
+would to convert to audio: ")
 
 yt = YouTube(youtube_link)
