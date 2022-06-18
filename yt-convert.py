ff77749e0c820baf3d771918c8b59fb19e0e93f0
James_Ying <yab.james@outlook.com>
2022-06-18 19:39:14 -0400

    Add audio download capability
    Downloads are placed in the downloads folder and set  to 160kbps

--- yt-convert.py
+++ yt-convert.py
@@ -1,4 +1,5 @@
 from pytube import YouTube
+from os.path import expanduser
 
 youtube_link = input("Input the youtube url of the video that you \
 would to convert to audio: ")
@@ -22,4 +23,8 @@ would to convert to audio: ")
         print(f'Is this the correct title of the youtube video? \n "{yt.title}" \n')
         confirm = input("Type Y/y for yes or N/n for no: ")
 
+# Download audio at 160kpbs bitrate
 yt = YouTube(youtube_link)
+yt.streams.filter(only_audio=True)
+audio_stream = yt.streams.get_by_itag(251)
+audio_stream.download(output_path=f"{expanduser('~')}/Downloads")
