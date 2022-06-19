34d24ddbf52789427b3ef3b2598c555bd7f99d37
James_Ying <yab.james@outlook.com>
2022-06-18 22:15:54 -0400

    Add convert_video()
    convert_video() encompasses all the contents of the file
    
    The purpose is to make it easier to make a module out of yt_convert

--- yt_convert.py
+++ yt_convert.py
@@ -1,30 +1,31 @@
 from pytube import YouTube
 from os.path import expanduser
 
-youtube_link = input("Input the youtube url of the video that you \
+def convert_video():
+    youtube_link = input("Input the youtube url of the video that you \
 would to convert to audio: ")
 
-yt = YouTube(youtube_link)
+    yt = YouTube(youtube_link)
 
-# confirmation 
-print(f'Is this the correct title of the youtube video? \n "{yt.title}" \n')
-confirm = input("Type Y/y for yes or N/n for no: ")
-proceed = False
+    # confirmation 
+    print(f'Is this the correct title of the youtube video? \n "{yt.title}" \n')
+    confirm = input("Type Y/y for yes or N/n for no: ")
+    proceed = False
 
-# loop through video confirmation process until user agrees
-while (proceed == False):
+    # loop through video confirmation process until user agrees
+    while (proceed == False):
 
-    if (confirm.lower() == 'y'):
-        proceed = True
+        if (confirm.lower() == 'y'):
+            proceed = True
 
-    elif (confirm.lower() == 'n'):
-        youtube_link = input("Retype the youtube url of the video that you \
+        elif (confirm.lower() == 'n'):
+            youtube_link = input("Retype the youtube url of the video that you \
 would to convert to audio: ")
-        print(f'Is this the correct title of the youtube video? \n "{yt.title}" \n')
-        confirm = input("Type Y/y for yes or N/n for no: ")
+            print(f'Is this the correct title of the youtube video? \n "{yt.title}" \n')
+            confirm = input("Type Y/y for yes or N/n for no: ")
 
-# Download audio at 160kpbs bitrate
-yt = YouTube(youtube_link)
-yt.streams.filter(only_audio=True)
-audio_stream = yt.streams.get_by_itag(251)
-audio_stream.download(output_path=f"{expanduser('~')}/Downloads")
+    # Download audio at 160kpbs bitrate
+    yt = YouTube(youtube_link)
+    yt.streams.filter(only_audio=True)
+    audio_stream = yt.streams.get_by_itag(251)
+    audio_stream.download(output_path=f"{expanduser('~')}/Downloads")
